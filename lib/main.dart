d283b124b54da3886edfb4ac69d601e6194f64a3
GeonH0 <h2jo369@naver.com>
2025-03-06 20:09:45 +0900

    Feat: MainTabView 생성 및 main.dart에 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:todaybills/view/home_view.dart';
+import 'package:todaybills/view/maintab_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting();
   runApp(MaterialApp(
-    home: homeView(),
+    home: MaintabView(),
   ));
 }
