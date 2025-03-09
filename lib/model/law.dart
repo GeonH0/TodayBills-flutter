ab8bd46216c641a45519cf0636c70d9a8180ec83
GeonH0 <h2jo369@naver.com>
2025-03-09 20:47:06 +0900

    Fix: Law 구조 변화 및 json 형식의 문자열로 직렬화

--- lib/model/law.dart
+++ lib/model/law.dart
@@ -1,9 +1,30 @@
+import 'dart:convert';
+
 class Law {
+  final String ID;
   final String title;
-  final String description;
 
   Law({
+    required this.ID,
     required this.title,
-    required this.description,
   });
+
+  String toJson() {
+    return jsonEncode({
+      'ID': ID,
+      'title': title,
+    });
+  }
+
+  factory Law.fromJson(String jsonString) {
+    final decoded = jsonDecode(jsonString);
+    if (decoded is Map<String, dynamic>) {
+      return Law(
+        ID: decoded['ID'],
+        title: decoded['title'],
+      );
+    } else {
+      throw Exception("Invalid JSON format for Law: $jsonString");
+    }
+  }
 }
