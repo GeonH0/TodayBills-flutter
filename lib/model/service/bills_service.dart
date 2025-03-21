24c21a589b90315571867752493e9beae6e87813
GeonH0 <h2jo369@naver.com>
2025-03-21 21:48:46 +0900

    Fix: billService에 fetchBillsByKeyword메서드 추가

--- lib/model/service/bills_service.dart
+++ lib/model/service/bills_service.dart
@@ -24,21 +24,49 @@ final class BillService {
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final dynamic rootKey = data.keys.first;
-
-      final List<dynamic> mainList = data[rootKey];
-      final rowPart = mainList[1] as Map<String, dynamic>;
-      final List<dynamic> rowData = rowPart['row'] as List<dynamic>;
-      final bills = rowData.map((item) {
-        final map = item as Map<String, dynamic>;
-        return Bill.fromJson(map);
-      }).toList();
-
-      return bills;
+      return _parseBills(response.body);
     } else {
       throw Exception(
           'Failed to load bills. Status code: ${response.statusCode}');
     }
+  }
+
+  Future<List<Bill>> fetchBillsByKeyword({
+    required String keyword,
+    required int pIndex,
+    required int age,
+  }) async {
+    final url = Uri.parse(baseUrl).replace(queryParameters: {
+      'key': apiKey,
+      'type': 'json',
+      'pIndex': pIndex.toString(),
+      'pSize': "100",
+      'AGE': age.toString(),
+      'BILL_NAME': keyword, // 법안명에 해당 키워드를 포함하는 법안 검색
+    });
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return _parseBills(response.body);
+    } else {
+      throw Exception(
+          'Failed to search bills. Status code: ${response.statusCode}');
+    }
+  }
+
+  List<Bill> _parseBills(String responseBody) {
+    final data = jsonDecode(responseBody);
+    final dynamic rootKey = data.keys.first;
+
+    final List<dynamic> mainList = data[rootKey];
+    final rowPart = mainList[1] as Map<String, dynamic>;
+    final List<dynamic> rowData = rowPart['row'] as List<dynamic>;
+    final bills = rowData.map((item) {
+      final map = item as Map<String, dynamic>;
+      return Bill.fromJson(map);
+    }).toList();
+
+    return bills;
   }
 }
