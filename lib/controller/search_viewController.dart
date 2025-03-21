7b526e313ecf991fa1c75ebb588ad579995697a7
GeonH0 <h2jo369@naver.com>
2025-03-21 21:49:44 +0900

    Fix: searchViewController 수정

--- lib/controller/search_viewController.dart
+++ lib/controller/search_viewController.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/cupertino.dart';
-import 'package:mvc_pattern/mvc_pattern.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todaybills/controller/list_viewController.dart';
+import 'package:todaybills/model/data/bill.dart';
+import 'package:todaybills/model/data/law.dart';
+import 'package:todaybills/model/service/search_service.dart';
 
-class SearchViewcontroller extends ControllerMVC {
-  List<String> filterdItems = [];
+class SearchViewController extends ListViewcontroller {
+  List<String> filteredItems = [];
   final List<String> keywords = ["부동산", "안전", "근로", "산업", "환경", "교육"];
-  List<String> searchList = []; // 검색 결과
+  List<Law> searchList = []; // 검색 결과
   List<String> recentSearches = []; // 최근 검색어 리스트
 
   final TextEditingController searchController = TextEditingController();
+  final SearchService searchService = SearchService();
 
-  SearchViewcontroller() {
+  SearchViewController() {
     searchController.addListener(onSearchChanged);
   }
   @override
   void initState() {
     super.initState();
     _loadRecentSearches();
+    loadFavorites();
   }
 
   @override
@@ -25,16 +30,32 @@ class SearchViewcontroller extends ControllerMVC {
     super.dispose();
   }
 
-  void onTapkeywords(String keyowrd) {
-    searchController.text = keyowrd;
-    _saveRecentSearch(keyowrd);
+  void onTapKeyword(String keyword) {
+    searchController.text = keyword;
+    _saveRecentSearch(keyword);
   }
 
-  void onSearchChanged() {
+  Law convertBillToLaw(Bill bill) {
+    return Law(
+      ID: bill.billId,
+      title: bill.billName,
+      age: bill.age,
+    );
+  }
+
+  void onSearchChanged() async {
     String query = searchController.text;
-    setState() {
-      filterdItems = [];
+    if (query.isEmpty) {
+      setState(() {
+        searchList = [];
+      });
+      return;
     }
+
+    final results = await searchService.searchBillsByKeywords(query);
+    setState(() {
+      searchList = results.map((bill) => convertBillToLaw(bill)).toList();
+    });
   }
 
   Future<void> _loadRecentSearches() async {
@@ -54,5 +75,29 @@ class SearchViewcontroller extends ControllerMVC {
       await prefs.setStringList('recent_searches', recentSearches);
       setState(() {});
     }
+  }
+
+  void deleteRecentSearch() {
+    final prefs = SharedPreferences.getInstance();
+    if (recentSearches.isNotEmpty) {
+      setState(() {
+        recentSearches.clear();
+      });
+    }
+  }
+
+  @override
+  Future<void> loadFavorites() {
+    return super.loadFavorites();
+  }
+
+  @override
+  Future<void> saveFavorites() {
+    return super.saveFavorites();
+  }
+
+  @override
+  void toggleFavorite(Law law) {
+    super.toggleFavorite(law);
   }
 }
